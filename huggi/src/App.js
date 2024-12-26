717b6795f90992ec240e34794e9f68ed9551a42c
Mohammed Kmail <kmail@kmail.at>
2024-12-26 13:16:50 +0100

    disclaimer add

--- huggi/src/App.js
+++ huggi/src/App.js
@@ -1,12 +1,14 @@
 
 import ImageGenerator from './components/imagegenerator';
 import './App.css';
+import Disclaimer from './components/disclaimer';
 
 function App() {
   return (
     <div className="App border border-primary bg-slate-50 w-full  px-4 py-2 rounded">
       
      <ImageGenerator  />
+     <Disclaimer />
     </div>
   );
 }
---
+++ huggi/src/components/disclaimer.js
@@ -0,0 +1,16 @@
+const Disclaimer = () => {
+    return (
+        <div className=" mx-auto p-4  text-sm">
+            <h2>Disclaimer</h2>
+
+            KMail.at cannot guarantee the functionality or quality of any images generated through its services. While we strive to maintain high standards and reliable processes, the accuracy, resolution, and overall quality of the generated images may vary and are dependent on several factors, including but not limited to input data, technical limitations, and user interactions.
+
+We expressly disclaim any responsibility for errors, omissions, or inconsistencies in the generated images and encourage users to verify and evaluate the output independently before use. By using our services, you acknowledge and accept these limitations.
+
+For further information or assistance, please contact our support team
+
+        </div>
+
+    );
+}
+export default Disclaimer;
