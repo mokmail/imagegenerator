8fb5668ca35812e0e604580b3450e06356fa373d
mohammed kmail <kmail@kmail.at>
2024-12-27 00:38:17 +0100

    small

--- huggi/src/components/disclaimer.js
+++ huggi/src/components/disclaimer.js
@@ -3,7 +3,7 @@ const Disclaimer = () => {
         <div className=" mx-auto p-4  text-sm w-full">
             <h2>Disclaimer</h2>
 
-            KMail.at cannot guarantee the functionality or quality of any images generated through its services. While we strive to maintain high standards and reliable processes, the accuracy, resolution, and overall quality of the generated images may vary and are dependent on several factors, including but not limited to input data, technical limitations, and user interactions.
+            Kmail.at cannot guarantee the functionality or quality of any images generated through its services. While we strive to maintain high standards and reliable processes, the accuracy, resolution, and overall quality of the generated images may vary and are dependent on several factors, including but not limited to input data, technical limitations, and user interactions.
 
 We expressly disclaim any responsibility for errors, omissions, or inconsistencies in the generated images and encourage users to verify and evaluate the output independently before use. By using our services, you acknowledge and accept these limitations.
 
